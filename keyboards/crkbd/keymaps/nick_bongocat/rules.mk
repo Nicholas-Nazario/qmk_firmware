3d574071b5826ca68310a9cd114e9b4cae96c4f8
Nicholas Nazario <Nazario9847@gmail.com>
2023-01-03 12:09:25 -0500

    Initial Keymap Commit w/ BongoCat

---
+++ keyboards/crkbd/keymaps/nick_bongocat/rules.mk
@@ -0,0 +1,6 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+WPM_ENABLE		= yes
+LTO_ENABLE      = yes
